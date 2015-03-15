f11d96425c6c94b0e89eb1be4f5e83845be282a2
Bryce Weiner <bryce@blocktech.com>
2015-03-15 03:01:25 -0400

    DM test, stupid error

--- bin/tipbot.js
+++ bin/tipbot.js
@@ -99,7 +99,7 @@ if(!fs.existsSync('./config/config.yml')) {
         });
     }
     client.stream('user', function (stream) {
-        console.log(data.direct_message);
+        console.log(data);
 		stream.on('data', function (data) {
 			if (data.follow)
             if (data.direct_message == null)
