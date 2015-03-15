0132f346aa8c8a8cca7bd8eaa6a51098fd7e9165
Bryce Weiner <bryce@blocktech.com>
2015-03-15 02:53:38 -0400

    DM test

--- bin/tipbot.js
+++ bin/tipbot.js
@@ -99,8 +99,10 @@ if(!fs.existsSync('./config/config.yml')) {
         });
     }
     client.stream('user', function (stream) {
-		stream.on('data', function (directMessage) {
-			console.log(directMessage);
+		stream.on('data', function (data) {
+            if (data.direct_message == null)
+                return;
+            console.log(data.direct_message);
 		});   
     })
     client.stream('statuses/filter', {track: 'darktipperbot'}, function (stream) {
